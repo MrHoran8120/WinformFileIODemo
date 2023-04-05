752d633b677ad87ed9d350f552f1c274001e7917
MrHoran8120 <garyhoran@users.noreply.github.com>
2023-04-05 12:36:57 +1000

    blah

--- WinformFileIODemo/Form1.Designer.cs
+++ WinformFileIODemo/Form1.Designer.cs
@@ -31,46 +31,94 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.streamReader = new System.Windows.Forms.Button();
+            this.streamWriter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 98);
+            this.button1.Location = new System.Drawing.Point(163, 129);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.Size = new System.Drawing.Size(91, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(392, 86);
+            this.button2.Location = new System.Drawing.Point(384, 129);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(103, 42);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Save File";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(293, 252);
+            this.richTextBox1.Location = new System.Drawing.Point(163, 177);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(324, 138);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Richtextbox load and save";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(522, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Examples of reading and writing to disk";
+            // 
+            // streamReader
+            // 
+            this.streamReader.Location = new System.Drawing.Point(163, 361);
+            this.streamReader.Name = "streamReader";
+            this.streamReader.Size = new System.Drawing.Size(90, 42);
+            this.streamReader.TabIndex = 5;
+            this.streamReader.Text = "Stream Reader";
+            this.streamReader.UseVisualStyleBackColor = true;
+            // 
+            // streamWriter
+            // 
+            this.streamWriter.Location = new System.Drawing.Point(384, 361);
+            this.streamWriter.Name = "streamWriter";
+            this.streamWriter.Size = new System.Drawing.Size(90, 42);
+            this.streamWriter.TabIndex = 5;
+            this.streamWriter.Text = "Stream Writer";
+            this.streamWriter.UseVisualStyleBackColor = true;
+            this.streamWriter.Click += new System.EventHandler(this.streamWriter_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(648, 630);
+            this.Controls.Add(this.streamWriter);
+            this.Controls.Add(this.streamReader);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +127,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button streamReader;
+        private System.Windows.Forms.Button streamWriter;
     }
 }
 
--- WinformFileIODemo/Form1.cs
+++ WinformFileIODemo/Form1.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp19
+namespace WinformFileIODemo
 {
     public partial class Form1 : Form
     {
@@ -18,7 +18,7 @@ namespace WindowsFormsApp19
         {
             InitializeComponent();
         }
-
+        #region Demo of richtextbox file open and file save
         private void button2_Click(object sender, EventArgs e)
         // Simple save of the contents of a richtextbox
         {
@@ -49,5 +49,51 @@ namespace WindowsFormsApp19
             }
 
         }
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region - This section handles the Filename and path dialog
+            OpenFileDialog myPrompt = new OpenFileDialog();
+            // Set the file name to myText.txt, set the type filter
+            // to text files, and set the initial directory to the 
+            // MyDocuments folder.
+            myPrompt.FileName = "myText";
+            // DefaultExt is only used when "All files" is selected from 
+            // the filter box and no extension is specified by the user.
+            myPrompt.DefaultExt = "txt";
+            myPrompt.Filter =
+                "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            myPrompt.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            DialogResult result = myPrompt.ShowDialog();
+            #endregion
+
+            if (result == DialogResult.OK)
+            {
+                // saving a richtextbox - note that the filename
+                // also contains the path.
+                richTextBox1.LoadFile(myPrompt.FileName, RichTextBoxStreamType.PlainText);
+            }
+
+        }
+        #endregion 
+        #region Demo of streamwriter and streamread - here we are writing the contents of a list to a text file
+
+        private void streamWriter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+        #endregion
+
+    
+
+        
     }
 }
